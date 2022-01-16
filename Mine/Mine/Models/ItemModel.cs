029193cd2cd31d1b12913ad652dee0214cc4cd5c
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-01-15 18:05:59 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -4,8 +4,13 @@ namespace Mine.Models
 {
     public class ItemModel
     {
+        //The ID for the Item
         public string Id { get; set; }
+
+        //The Display text for the Item
         public string Text { get; set; }
+
+        //The Description for the Item
         public string Description { get; set; }
 
         //The value of the Item +9 Damage 
