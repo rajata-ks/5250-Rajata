41ffffd532222ac9f09fd157333babcae4e7979c
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-02-07 11:12:05 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_()
+        {
+            //Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert
+
+            Assert.Pass();
+        }
+    }
+}
