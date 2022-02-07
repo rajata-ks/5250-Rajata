7a2180ab405e514807f8b9d176ce1c0ef684e50f
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-02-07 13:15:39 -0800

    Added RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15 in DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -118,6 +118,21 @@ namespace UnitTests.Helpers
         }
 
         [Test]
-    
+        public void RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15()
+        {
+            //Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
+
+            //Act
+            var result = DiceHelper.RollDice(3, 10);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            //Assert
+            Assert.AreEqual(15, result);
+
+        }
     }
 }
